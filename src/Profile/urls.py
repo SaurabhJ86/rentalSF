8bf154e13d5e4971f9f7a7268af68a00f0adee7f
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2019-03-27 13:23:49 +0530

    Accounts Page

--- src/Profile/urls.py
+++ src/Profile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import signup
+from .views import account,signup
 
 app_name='Profile'
 
 urlpatterns = [
 	path('',signup,name='profile-signup'),
+	path('account/',account,name='profile-account'),
 ]
--- src/Profile/views.py
+++ src/Profile/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import login,authenticate
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
 
 from .forms import SignUpForm
 
@@ -23,5 +25,15 @@ def signup(request):
 	context = {
 		"form":form
 	}
+
+	return render(request,template,context)
+
+
+@login_required(login_url="login")
+def account(request):
+	username = request.user
+	template = "accounts.html"
+
+	context = {}
 
 	return render(request,template,context)
